8939c9d3ddda3fbf5fffabbe1b875221cd0ea36c
shtechup <168064368+shtechup@users.noreply.github.com>
2024-05-14 04:30:12 +0000

    update with js

---
+++ script.js
@@ -0,0 +1,35 @@
+var age = 20
+age = 65
+
+var username = "singhui"
+var password = "techup"
+
+var isMale = false
+var isLoggedIn = true
+
+console.log(age)
+
+var blog_posts = ["Today was a good day", "The sky is blue", 
+"It's raining heavily now"]
+
+console.log(username)
+console.log(password)
+console.log(isMale)
+console.log(isLoggedIn)
+console.log(blog_posts)
+
+if (isMale == true) {
+    console.log("You are male")
+
+  }  else {
+        console.log ("You are female")
+    }
+
+    while (age < 70) {
+        console.log("Your age is " + age );
+        age = age + 1;
+    }
+    function sayHello() {
+        alert("Hello everyone");
+    }
+        
